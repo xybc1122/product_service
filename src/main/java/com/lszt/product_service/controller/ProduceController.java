d483c459c63b4220866cc8a9ca5516d0a6192dc5
cloud <451597529@qq.com>
2018-08-30 22:57:20 +0800

    2018/8/30修改

--- src/main/java/com/lszt/product_service/controller/ProduceController.java
+++ src/main/java/com/lszt/product_service/controller/ProduceController.java
@@ -1,7 +1,10 @@
 package com.lszt.product_service.controller;
 
+import com.lszt.product_service.domain.Product;
 import com.lszt.product_service.service.ProductService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/product")
 public class ProduceController {
+    @Value("${server.port}")
+    private String port;
     @Autowired
     private ProductService productService;
 
@@ -17,8 +22,12 @@ public class ProduceController {
         return productService.listProduct();
     }
 
-    @RequestMapping("id")
-    public Object findById(@RequestParam("id") int id) {
-        return productService.findById(id);
+    @RequestMapping("find")
+    public Product findById(@RequestParam("id") int id) {
+        Product product = productService.findById(id);
+        Product result = new Product();
+        BeanUtils.copyProperties(product, result);
+        result.setName(result.getName() + "data from port=" + port);
+        return result;
     }
 }
