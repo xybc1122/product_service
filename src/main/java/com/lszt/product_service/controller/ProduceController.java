35c46c721eb99b183ac487f0e6686767d28af0d1
xc <451597529@qq.com>
2018-09-06 17:23:42 +0800

    2018/9/6修改

--- src/main/java/com/lszt/product_service/controller/ProduceController.java
+++ src/main/java/com/lszt/product_service/controller/ProduceController.java
@@ -5,6 +5,7 @@ import com.lszt.product_service.service.ProductService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +14,13 @@ import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("api/product")
+//分布式配置中心动态刷新注解 RefreshScope
+//@RefreshScope
 public class ProduceController {
     @Value("${server.port}")
     private String port;
+    @Value("${env}")
+    private String env;
 
     @Autowired
     private ProductService productService;
@@ -37,7 +42,7 @@ public class ProduceController {
         Product result = new Product();
         //拷贝对象 伪代码
         BeanUtils.copyProperties(product, result);
-        result.setName(result.getName() + "data from prot=" + port);
+        result.setName(result.getName() + "data from prot=" + port + " env=" + env);
         return result;
     }
 }
